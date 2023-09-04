61ab59699efb6a4b1414e590869a89fdc076aa3a
Abdullah Mahmoud <abdullah.mahmoud.f196@gmail.com>
2023-09-04 15:15:44 +0300

    new update from abdullah

--- GithubLab/src/App.jsx
+++ GithubLab/src/App.jsx
@@ -1,5 +1,6 @@
 
 import Navbar from './Component/Navbar/Navbar'
+import About from './Component/Abdullah/about';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Navbar/>
+     <About/>
     </>
   )
 }
